e2e768eb84e3e2133622c13ace17dd177e6aaa9f
Ju Yuanmao <juyuanmao@outlook.com>
2019-04-15 16:03:20 +0800

    finish doc comment

--- user.cpp
+++ user.cpp
@@ -3,11 +3,17 @@
 
 #include "user.hpp"
 
-void password(const user_unit * user, char * passwd, size_t str_size) {
+/// \brief 由用户名查询密码
+/// \param user 用户
+/// \param out_string 用于储存结果的字符串
+/// \param str_size 储存区域的大小
+void password(const user_unit * user, char * out_string, size_t str_size) {
 
 }
 
-
-void change_password(const user_unit * user, char * passwd, size_t str_size) {
+/// \brief 修改用户密码
+/// \param user 用户
+/// \param new_password 储存新密码的字符串
+void change_password(const user_unit * user, char * new_password) {
 
 }
